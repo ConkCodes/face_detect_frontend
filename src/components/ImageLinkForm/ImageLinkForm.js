8d498183f9921c0c549ca4e93ff82e50d79793b0
Connor <connoroakvillea32@hotmail.com>
2021-11-15 13:25:59 -0500

    last text style changes

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -4,7 +4,7 @@ const ImageLinkForm = (props) => {
 
     return(
         <div className="flex fd-column ai-center">
-            <p className="mb-0 c-gradient b">Enter an Image Link for the Magic Brain to Detect Human Faces</p>
+            <p className="mb-0 c-gradient b fs-150">Enter an Image Link for the Magic Brain to Detect Human Faces</p>
             <div className="w-512 flex p-16 m-16 card">
                 <input onChange={props.onInputChange} type="text" className="w-70p input"/>
                 <button onClick={props.onDetectClick} className="w-30p button">Detect</button>
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,8 +4,8 @@ const Rank = (props) => {
 
     return(
         <div className="ta-center">
-            <p className="c-gradient b mt-32 mb-0">{props.name}, Your Current Entry Count Is...</p>
-            <p className="c-gradient b mt-0 mb-0">{props.entries}</p>
+            <p className="c-gradient b fs-150 mt-32 mb-0">{props.name}, Your Current Entry Count Is...</p>
+            <p className="c-gradient b fs-200 mt-0 mb-0">{props.entries}</p>
         </div>
     );
 
