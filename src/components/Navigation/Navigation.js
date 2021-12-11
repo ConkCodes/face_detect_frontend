d4b57cab566787ad32507dfce8f118320996bcbe
connor <connoroakvillea32@hotmail.com>
2021-12-11 13:15:26 -0500

    cleaned up code in navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,36 +3,33 @@ import Link from "../Link/Link.js";
 
 const Navigation = (props) => {
 
-    if (props.route === "signIn") {
+    let classes;
+
+    if (props.route === "signIn" || props.route === "signUp") {
+
+        if (props.route === "signIn") classes = ["navLink1", "navLink2"];
+        else if (props.route === "signUp") classes = ["navLink2", "navLink1"];
+
         return (
             <nav className="nav">
-                <Link onClick={() => props.onRouteChange("signIn")} className="navLink1" text="Sign In"/>
-                <Link onClick={() => props.onRouteChange("signUp")} className="navLink2" text="Sign Up"/>
+                <Link onClick={() => props.onRouteChange("signIn")} className={classes[0]} text="Sign In"/>
+                <Link onClick={() => props.onRouteChange("signUp")} className={classes[1]} text="Sign Up"/>
             </nav>
         );
-    } else if (props.route === "signUp") {
+
+    } else {
+
+        if (props.route === "home") classes = ["navLink1", "navLink2", "navLink2"];
+        else if (props.route === "profile") classes = ["navLink2", "navLink1", "navLink2"];
+
         return (
             <nav className="nav">
-                <Link onClick={() => props.onRouteChange("signIn")} className="navLink2" text="Sign In"/>
-                <Link onClick={() => props.onRouteChange("signUp")} className="navLink1" text="Sign Up"/>
+                <Link onClick={() => props.onRouteChange("home")} className={classes[0]} text="Home"/>
+                <Link onClick={() => props.onRouteChange("profile")} className={classes[1]} text="Profile"/>
+                <Link onClick={() => props.onRouteChange("signOut")} className={classes[2]} text="Sign Out"/>
             </nav>
         );
-    } else if (props.route === "home") {
-        return (
-            <nav className="nav">
-                <Link onClick={() => props.onRouteChange("home")} className="navLink1" text="Home"/>
-                <Link onClick={() => props.onRouteChange("profile")} className="navLink2" text="Profile"/>
-                <Link onClick={() => props.onRouteChange("signOut")} className="navLink2" text="Sign Out"/>
-            </nav>
-        );
-    } else if (props.route === "profile") {
-        return (
-            <nav className="nav">
-                <Link onClick={() => props.onRouteChange("home")} className="navLink2" text="Home"/>
-                <Link onClick={() => props.onRouteChange("profile")} className="navLink1" text="Profile"/>
-                <Link onClick={() => props.onRouteChange("signOut")} className="navLink2" text="Sign Out"/>
-            </nav>
-        );
+
     }
 
 }
